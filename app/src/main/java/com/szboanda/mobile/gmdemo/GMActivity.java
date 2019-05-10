2cbe529f022860d3a52095b257f8e378eb1228e0
suyang075100 <378870185@qq.com>
2019-05-10 16:17:39 +0800

    Initial commit

---
+++ app/src/main/java/com/szboanda/mobile/gmdemo/GMActivity.java
@@ -0,0 +1,13 @@
+package com.szboanda.mobile.gmdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
